b929d0950e34508d2b205cd150115ef3e1ef889e
Todd Yandell <tyandell@gmail.com>
2018-06-09 21:56:26 -0500

    Reformat comments

--- install.sh
+++ install.sh
@@ -3,10 +3,11 @@
 # Install dotfiles.
 #
 
-# Install install dependencies.
+# Install installer dependencies.
 brew install stow
 
 # fish
+# ====
 brew install fish
 curl -L -o ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
 stow --no-folding fish
@@ -14,21 +15,25 @@ mkdir ~/bin
 fish -c fisher
 
 # neovim
+# ======
 brew install neovim
 curl -L -o ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 stow --no-folding neovim
 nvim +PlugInstall +qa
 
 # gnupg
+# =====
 brew install gnupg
 brew install pinentry-mac
 stow --no-folding gnupg
 
 # git
+# ===
 brew install git
 stow --no-folding git
 curl -L -o ~/.config/git/ignore --create-dirs https://raw.githubusercontent.com/github/gitignore/master/Global/macOS.gitignore
 
 # direnv
+# ======
 brew install direnv
 fish -c fisher omf/direnv
