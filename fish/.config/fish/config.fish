63c5826fbfe66e47d31db3f41bcc7d92f58bbafb
Todd Yandell <tyandell@gmail.com>
2025-02-18 10:00:33 -0600

    Update for new asdf

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -18,4 +18,12 @@ direnv hook fish | source
 
 # asdf
 # ====
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
